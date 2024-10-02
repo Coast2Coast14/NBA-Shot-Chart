3f2f84d6a9fd61234b77cd5c7f6628346f90c910
Coast2Coast14 <wootenelijah29@gmail.com>
2024-10-02 08:33:27 -0700

    Resetting initialization

---
+++ nba_shot_chart.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from nba_api.stats.endpoints import shotchartdetail
+from nba_api.stats.static import players
+import matplotlib.pyplot as plt
+from mplbasketball import Court
+import pandas as pd
+
+# Testing changes
+st.title("NBA Shot Charts")
+st.subheader("Filter on any team and player to get their shot chart data")
+
+
+df = pd.read_csv("all_shots_df_2023-2024.csv")
+
+# Convert inches to feet
+df["LOC_X"] = df["LOC_X"] / 12
+df["LOC_Y"] = df["LOC_Y"] / 12
+
+# Flip the Y-axis to match the vertical "vu" court
+df["LOC_X"] = -df["LOC_X"] * 1.2
+df["LOC_Y"] = -df["LOC_Y"] + 39
+
+
+court = Court(court_type="nba", origin="center", units="ft")
+fig, ax = court.draw(showaxis=True)
+
+
+team = st.selectbox("Select a team", df["TEAM_NAME"].sort_values().unique(), index=None)
+player = st.selectbox(
+    "Select a player",
+    df[df["TEAM_NAME"] == team]["PLAYER_NAME"].sort_values().unique(),
+    index=None,
+)
+
+
+def filter_data(df, team, player):
+    if team:
+        df = df[df["TEAM_NAME"] == team]
+
+    if player:
+        df = df[df["PLAYER_NAME"] == player]
+
+        court = Court(court_type="nba", origin="center", units="ft")
+        fig, ax = court.draw(orientation="vu")
+
+        def plot_shots(df, ax, court):
+            # for x in df.to_dict(orient="records"):
+            #    ax.scatter(x=(df["LOC_X"]), y=(df["LOC_Y"]), s=1.5, zorder=0)
+
+            # Plot the heat map using hexbin (you can adjust gridsize and colormap)
+            hb = ax.hexbin(
+                df["LOC_X"], df["LOC_Y"], gridsize=30, cmap="cividis", zorder=0
+            )
+
+            # Add a color bar to show the density of shots
+            plt.colorbar(hb, ax=ax, label="Shot Frequency")
+
+        plot_shots(df, ax, court)
+
+        st.pyplot(fig)
+
+    return df
+
+
+filtered_df = filter_data(df, team, player)
